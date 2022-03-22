b4f3551ba73f43f83fc2db81203817a392c6b9d3
imJanice <68151161+imJanice@users.noreply.github.com>
2022-03-22 10:58:10 +0800

    Time: 50 ms (67.62%), Space: 21 MB (99.12%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     string getSmallestString(int n, int k) {
         int tg = 26;
-        int less = k;
         char dict[26] = {'a','b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
         string ans(n, 'a');
         for(int i = 1;i <= n; i++){
-            if((less - (n - i)) < tg){
-                tg = less - (n - i);
+            if((k - (n - i)) < tg){
+                tg = k - (n - i);
             }
-            less -= tg;
+            k -= tg;
             ans[n - i] = dict[tg - 1];
             if(tg == 1){
                 break;
