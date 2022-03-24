0473364293f469f480fbe03ffdae8196087a0f76
imJanice <68151161+imJanice@users.noreply.github.com>
2022-03-24 21:14:05 +0800

    Time: 132 ms (28.39%), Space: 42 MB (57.42%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int left = 0;
+        int right = people.size() - 1;
+        int ans = 0;
+        sort(people.begin(), people.end());
+        while(left < right){
+            if(people[right] == limit){
+                right--;
+            }else if(people[left] + people[right] > limit){
+                right--;
+            }else{
+                // people[left] + people[right] < limit
+                right--;
+                left++;
+            }
+            ans++;
+            if(left == right){
+                ans++;
+                break;
+            }
+        }
+        return ans;
+    }
+};
