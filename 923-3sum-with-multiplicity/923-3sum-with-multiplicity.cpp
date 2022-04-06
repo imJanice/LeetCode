c49b796d1479bfa87ad66090b2656e24ce9009bd
imJanice <68151161+imJanice@users.noreply.github.com>
2022-04-06 23:36:05 +0800

    Time: 1570 ms (5.11%), Space: 10.7 MB (32.99%) - LeetHub

---
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int threeSumMulti(vector<int>& arr, int target) {
+        unordered_map<int,int>mp; //Using unordered map
+        int res = 0;
+        int mod = 1e9+7;
+        for(int i=0;i<arr.size();++i)
+        {
+            res = (res + mp[target - arr[i]]) % mod;
+
+            for(int j=0;j<i;++j)
+            {
+                mp[arr[i] + arr[j]]++;
+            }
+        }
+        return res;
+    }
+};
