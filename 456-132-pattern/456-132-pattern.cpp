7ba5a989c421f92d844846ac1941e7a13d55e90b
imJanice <68151161+imJanice@users.noreply.github.com>
2022-05-07 18:07:14 +0800

    Time: 75 ms (84.58%), Space: 38.2 MB (84.03%) - LeetHub

---
+++ 456-132-pattern/456-132-pattern.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        // i < j < k
+        // nums[i] < nums[k] < nums[j]
+        // stack : first in last out
+        stack<int> s;
+        int k = INT_MIN;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(nums[i] < k) return true;
+            while(!s.empty() && nums[i] > s.top()){
+                k = s.top();
+                s.pop();
+            }
+            s.push(nums[i]);
+        }
+        return false;
+    }
+};
