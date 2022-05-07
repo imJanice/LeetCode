ac54a02a584d48cb670d373024657affb0c043eb
imJanice <68151161+imJanice@users.noreply.github.com>
2022-05-07 18:30:30 +0800

    Time: 112 ms (34.07%), Space: 38.2 MB (68.61%) - LeetHub

--- 456-132-pattern/456-132-pattern.cpp
+++ 456-132-pattern/456-132-pattern.cpp
@@ -4,6 +4,10 @@ public:
         // i < j < k
         // nums[i] < nums[k] < nums[j]
         // stack : first in last out
+        // put 2 inside stack
+        // check if 3 greater than 2
+        // get max 2 inside stack
+        // if 1 < 2 than return true
         stack<int> s;
         int k = INT_MIN;
         for(int i=nums.size()-1;i>=0;i--){
