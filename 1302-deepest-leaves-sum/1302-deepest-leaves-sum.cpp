19c44ec41871b1e18f541f2321d447fad9a712b3
imJanice <68151161+imJanice@users.noreply.github.com>
2022-05-15 12:22:34 +0800

    Time: 114 ms (75.53%), Space: 59.8 MB (53.32%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -11,23 +11,46 @@
  */
 class Solution {
 public:
-    map<int, int> m;
+    vector<int> v;
     void visit(TreeNode* root, int deep){
+        if(deep == v.size()){
+            v.push_back(root->val);
+        }else{
+            v[deep] += root->val;
+        }
         if(root->right != NULL){
            visit(root->right, deep+1);
         }
-        //cout<<root->val<<endl;
         if(root->left != NULL){
            visit(root->left, deep+1);
-        }
-        if(m.find(deep) != m.end()){
-            m[deep] += root->val;
-        }else{
-            m[deep] = root->val;
         }
     }
     int deepestLeavesSum(TreeNode* root) {
         visit(root, 0);
-        return m.rbegin()->second;
+        return v[v.size()-1];
     }
 };
+
+
+/*  Runtime : 241 ms
+    Memory  : 59.9 MB
+map<int, int> m;
+void visit(TreeNode* root, int deep){
+    if(root->right != NULL){
+       visit(root->right, deep+1);
+    }
+    //cout<<root->val<<endl;
+    if(root->left != NULL){
+       visit(root->left, deep+1);
+    }
+    if(m.find(deep) != m.end()){
+        m[deep] += root->val;
+    }else{
+        m[deep] = root->val;
+    }
+}
+int deepestLeavesSum(TreeNode* root) {
+    visit(root, 0);
+    return m.rbegin()->second;
+}
+*/
