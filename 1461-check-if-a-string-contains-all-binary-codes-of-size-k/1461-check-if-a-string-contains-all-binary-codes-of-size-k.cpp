03a4a3c50188945f45659f23e66ccc1bca7879ad
imJanice <68151161+imJanice@users.noreply.github.com>
2022-05-31 22:35:36 +0800

    Time: 630 ms (32.70%), Space: 71.8 MB (42.77%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        if(s.size() < k) return false;
-        string tmp;
+        int n = s.size();
         unordered_set<string> allBiType;
-        for(int i=0;i<s.size()-k+1;i++){
-            tmp = "";
-            for(int j=i;j<i+k;j++){
-                tmp += s[j];
-            }
-            allBiType.insert(tmp);
-        }
-        if(pow(2,k) == allBiType.size()){
-            return true;
-        }else{
-            return false;
-        }
+        for (int i = 0; i < n - k + 1; i++)
+            allBiType.insert(s.substr(i, k));
+        return allBiType.size() == pow(2, k) ? true : false;
     }
 };
+
+
+
+/*
+----- 05/31/2022 22:32	Accepted	810 ms	67.1 MB	cpp
+bool hasAllCodes(string s, int k) {
+    if(s.size() < k) return false;
+    string tmp;
+    unordered_set<string> allBiType;
+    for(int i=0;i<s.size()-k+1;i++){
+        tmp = "";
+        for(int j=i;j<i+k;j++){
+            tmp += s[j];
+        }
+        allBiType.insert(tmp);
+    }
+    if(pow(2,k) == allBiType.size()){
+        return true;
+    }else{
+        return false;
+    }
+}
+*/
